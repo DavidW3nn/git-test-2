edfa454d49ce7cc52ee58bbca16027920d32f397
DavidW3nn <davidwn26049@gmail.com>
2023-12-18 11:30:52 +0700

    Menambahkan data mahasiswa di index.php

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 <body>
     <h1>Mencoba Git Remote</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus consectetur ab amet ea saepe velit autem nulla deleniti et, cupiditate, dolores esse neque sint laborum? Veritatis id repellendus facilis quia explicabo maiores, eos consequatur rerum libero at nemo ut ex nesciunt ullam itaque corrupti saepe alias sint quis odit numquam et incidunt inventore quo? Hic error, qui voluptatum dolores aperiam quia ullam quam, provident fugiat, asperiores cumque beatae! Tempora aperiam rem fugit corporis, voluptatibus provident quasi reiciendis facere sapiente quam quas ut et quidem placeat porro impedit aspernatur eius delectus ipsam aliquid dolores veritatis ratione ipsa dolor. Facilis labore neque illo, asperiores facere distinctio, tenetur aliquam velit porro iusto explicabo! Rerum adipisci quos nostrum, minima corporis veniam placeat cum, veritatis voluptatum quis animi delectus! Dolor ipsa placeat fugit, deserunt impedit eaque doloremque laudantium nulla temporibus cupiditate dolore magni? Pariatur inventore cumque officiis porro, voluptatum libero, temporibus ea aperiam corporis numquam obcaecati debitis dolorum amet aut repudiandae doloribus necessitatibus. Corrupti ut esse in facilis amet aperiam magnam exercitationem pariatur repudiandae numquam. Voluptatum esse ex explicabo neque atque iusto voluptates repellat suscipit consequuntur quibusdam iure quo ab repellendus molestiae adipisci impedit sequi sunt voluptas libero consequatur quaerat maxime, officia optio! Aliquam maxime error doloremque adipisci repudiandae, eligendi ipsa, delectus voluptatibus incidunt, aspernatur iure perspiciatis nisi odit necessitatibus sed molestias? Reiciendis nostrum explicabo ab, iusto est iure labore eligendi at perferendis laudantium molestias aspernatur hic animi excepturi dolores autem tenetur eius expedita nisi consectetur amet illo aliquid. Quod impedit sint fugiat consequuntur? Molestias dignissimos accusantium blanditiis. Repudiandae iste, saepe blanditiis aliquid ullam magnam voluptatem accusantium harum doloremque molestiae quos iure aliquam libero. Nam accusamus aliquid deserunt eaque consequuntur consectetur odit, sed sunt voluptatum, asperiores blanditiis tempora. Iure, vero exercitationem quis eveniet mollitia est. Eveniet fugit dolores ullam suscipit. Officiis quae doloremque doloribus laborum!</p>
+
+    <h1>Data Mahasiswa</h1>
+    <?php
+    for ($i = 0; $i < 10; $i++) {
+        echo "<p>Mahasiswa ke-$i</p>";
+    }
+    ?>
 </body>
 
 </html>
